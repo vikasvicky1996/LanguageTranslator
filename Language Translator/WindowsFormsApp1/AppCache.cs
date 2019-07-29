c91298e7839ecfedb382f8eaa4c56bf9e6a36e4c
vicky <52102709+vikasvicky1996@users.noreply.github.com>
2019-07-28 21:34:26 -0700

    Add files via upload

---
+++ Language Translator/WindowsFormsApp1/AppCache.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowsFormsApp1
+{
+    class AppCache
+    {
+        public static string API { get; } = @"trnsl.1.1.20190713T135845Z.c92248bfb60a4114.7725533a9e8977a4dc19ef0b4460f7b1705fd26a";
+        public static string UrlGetAvailableLanguages { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/getLangs?key={0}&ui={1}";
+        public static string UrlDetectSrcLanguage { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/detect?key={0}&text={1}";
+        public static string UrlTranslateLanguage { get; } = @"https://translate.yandex.net/api/v1.5/tr.json/translate?key={0}&text={1}&langs={2}";
+
+    }   
+}
---
+++ Language Translator/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+
+namespace WindowsFormsApp1
+{
+
+
+    public partial class Form1 : Form
+    {
+        private List<String> AvailLangs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           var response = Requestservice(string.Format(AppCache.UrlDetectSrcLanguage, AppCache.API, textBox2.Text));
+            var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+
+            var statusCode = dict["code"].ToString();
+            if  (statusCode.Equals("200"))
+            {
+                label1.Text = dict["lang"].ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var response = Requestservice(string.Format(AppCache.UrlTranslateLanguage,AppCache.API,textBox2.Text,AvailLangs[comboBox1.SelectedIndex]));
+            var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+            var statusCode = dict["code"].ToString();
+            if (statusCode.Equals("200"))
+            {
+
+               textBox1.Text = string.Join(",",dict["text"]);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var response = Requestservice(string.Format(AppCache.UrlGetAvailableLanguages,AppCache.API,label1.Text));
+            var dict = JsonConvert.DeserializeObject<IDictionary>(response.Content);
+            foreach(DictionaryEntry entry in dict)
+            {
+                if (entry.Key.Equals("langs"))
+                {
+                    var availableConverts = (JObject)entry.Value;
+                    AvailLangs = new List<string>();
+
+                    comboBox1.Items.Clear();
+                    foreach (var Lang in availableConverts)
+                    {
+                        if(!Lang.Equals(label1.Text))
+                        {
+                            comboBox1.Items.Add(Lang.Value);
+                            AvailLangs.Add(Lang.Key);
+
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        private IRestResponse Requestservice(string strUrl)
+        {
+            var client = new RestClient()
+            {
+                BaseUrl = new Uri(strUrl)
+
+            };
+            var request = new RestRequest()
+            {
+                Method = Method.GET
+            };
+            return client.Execute(request);
+
+         
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
